0055f781c7c902c335878c04c48631e7fbb3305e
ZachHoskins <zach9414@gmail.com>
2019-09-28 22:32:45 -0500

    Updated, but grid still will not display

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,58 @@
-const container = document.getElementById("container");
+const gridContainer = document.querySelector('.container');
+const newGridButton = document.querySelector('.reset');
+let gridSize = 16;
 
-// function makeRows(rows, cols) {
-//     container.style.setProperty('--grid-rows', rows);
-//     container.style.setProperty('--grid-cols', cols);
-//     for (c = 0; c < (rows * cols); c++) {
-//         let cell = document.createElement("div");
-//         cell.innerText = (c + 1);
-//         container.appendChild(cell).className = "grid-item";
-//     };
-// };
+function createGrid() {
+    let gridRows = gridSize;
 
-// makeRows(16, 16);
+    for (let i = 0; i < gridRows.length; i++) {
+        const row = document.createElement('div');
+        let gridColumns = gridSize;
+
+        row.classList.add('grid-row');
+        console.log("row: " + i);
+        gridContainer.appendChild(row);
+
+        for (let j = 0; j < gridColumns.length; j++) {
+            const column = document.createElement('div');
+            column.classList.add('grid-column');
+            console.log("column: " + j);
+            row.appendChild(column);
+        }
+    }
+}
+
+
+function colorGrid(e) {
+    if (e.target.classList[0] === 'grid-column') {
+        e.target.classList.add('black');
+    }
+}
+
+
+function clearGrid() {
+    const pixels = document.querySelectorAll('.grid-column');
+    pixels.forEach(element => {
+
+    });
+    // for (let i = 0; i < pixels.length; i++) {
+    //     pixels[i].classList.remove('black');
+    // }
+}
+
+function newGrid() {
+    gridSize = 0;
+    clearGrid();
+
+    while ((gridSize < 1) || (gridSize > 100)) {
+        gridSize = prompt('Enter a new grid size:');
+        gridSize = parseInt(gridSize, 10);
+    }
+
+    gridContainer.textContent = '';
+    createGrid();
+}
+
+createGrid();
+gridContainer.addEventListener('mouseover', colorGrid);
+newGridButton.addEventListener('click', newGrid);
