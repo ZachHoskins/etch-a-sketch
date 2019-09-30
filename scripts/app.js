1214cee9152682f0a7be98e79bb6ceec4049481f
ZachHoskins <zach9414@gmail.com>
2019-09-29 21:44:18 -0500

    Finished Etch-A-Sketch with a Dark Theme

--- scripts/app.js
+++ scripts/app.js
@@ -1,27 +1,28 @@
-const gridContainer = document.querySelector('.container');
-const newGridButton = document.querySelector('.reset');
+/* CREATE GRID */
+
+const gridContainer = document.querySelector('.grid-container');
+const newGridButton = document.querySelector('.new-grid');
 let gridSize = 16;
 
 function createGrid() {
     let gridRows = gridSize;
+    let gridColumns = gridSize;
 
-    for (let i = 0; i < gridRows.length; i++) {
+    for (let i = 0; i < gridRows; i++) {
         const row = document.createElement('div');
-        let gridColumns = gridSize;
-
         row.classList.add('grid-row');
-        console.log("row: " + i);
         gridContainer.appendChild(row);
 
-        for (let j = 0; j < gridColumns.length; j++) {
+
+        for (let j = 0; j < gridColumns; j++) {
             const column = document.createElement('div');
             column.classList.add('grid-column');
-            console.log("column: " + j);
             row.appendChild(column);
         }
     }
 }
 
+/* COLOR GRID FUNCTIONALITY */
 
 function colorGrid(e) {
     if (e.target.classList[0] === 'grid-column') {
@@ -29,29 +30,29 @@ function colorGrid(e) {
     }
 }
 
+/* CLEAR GRID FUNCTIONALITY */
 
 function clearGrid() {
     const pixels = document.querySelectorAll('.grid-column');
-    pixels.forEach(element => {
 
-    });
-    // for (let i = 0; i < pixels.length; i++) {
-    //     pixels[i].classList.remove('black');
-    // }
+    for (let i = 0; i < pixels.length; i++) {
+        pixels[i].classList.remove('black');
+    }
 }
 
 function newGrid() {
     gridSize = 0;
     clearGrid();
 
-    while ((gridSize < 1) || (gridSize > 100)) {
-        gridSize = prompt('Enter a new grid size:');
+    while ((gridSize < 1) || (gridSize > 64)) {
+        gridSize = prompt('Enter a new grid size from 1-64');
         gridSize = parseInt(gridSize, 10);
     }
 
     gridContainer.textContent = '';
     createGrid();
 }
+
 
 createGrid();
 gridContainer.addEventListener('mouseover', colorGrid);
